b7641cd0094c2bddd55fa18c3645847bb4be6d33
ablanchard17 <ablanchard1010@gmail.com>
2014-09-16 11:22:34 -0700

    Added XMLBaseFile to Assets.Scripts.Data

--- Assets/Scripts/Data/XMLBaseFile.cs
+++ Assets/Scripts/Data/XMLBaseFile.cs
@@ -8,127 +8,146 @@ using System.IO;
 using System.Security.Cryptography;
 using System.Text;
 
-public class XMLBaseFile : System.Object {
-	private static readonly string _base_path = Application.persistentDataPath + "/"; 	// Base path for data files.
-	private static readonly string _key = "12345678901234567890123456789012";			// Key
-	private static readonly string _extension = ".xml";
+namespace Assets.Scripts.Data
+{
+    public class XMLBaseFile : System.Object
+    {
+        private static readonly string _base_path = Application.persistentDataPath + "/"; 	// Base path for data files.
+        private static readonly string _key = "12345678901234567890123456789012";			// Key
+        private static readonly string _extension = ".xml";
 
-	/// <summary>
-	/// Checks if the file name exists.
-	/// </summary>
-	/// <returns><c>true</c>, if file exits, <c>false</c> otherwise.</returns>
-	/// <param name="fileName">File name.</param>
-	protected static bool FileExits(string fileName) {
-		if(File.Exists(_base_path + fileName + XMLBaseFile._extension)) {
-			return true;
-		} else {
-			return false;
-		}
-	}
+        /// <summary>
+        /// Checks if the file name exists.
+        /// </summary>
+        /// <returns><c>true</c>, if file exits, <c>false</c> otherwise.</returns>
+        /// <param name="fileName">File name.</param>
+        protected static bool FileExits(string fileName)
+        {
+            if (File.Exists(_base_path + fileName + XMLBaseFile._extension))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
 
-	/// <summary>
-	/// Creates the XML file (NON-Encrypted)
-	/// </summary>
-	/// <param name="fileName">File name.</param>
-	/// <param name="obj">Object.</param>
-	protected static void CreateXMLFile(string fileName, System.Object obj) {
-		XmlSerializer serializer = new XmlSerializer(obj.GetType());
+        /// <summary>
+        /// Creates the XML file (NON-Encrypted)
+        /// </summary>
+        /// <param name="fileName">File name.</param>
+        /// <param name="obj">Object.</param>
+        protected static void CreateXMLFile(string fileName, System.Object obj)
+        {
+            XmlSerializer serializer = new XmlSerializer(obj.GetType());
 
-		using(FileStream stream = new FileStream(XMLBaseFile._base_path + fileName + XMLBaseFile._extension, FileMode.Create)) {
-			serializer.Serialize(stream, obj);
-		}
-	}
+            using (FileStream stream = new FileStream(XMLBaseFile._base_path + fileName + XMLBaseFile._extension, FileMode.Create))
+            {
+                serializer.Serialize(stream, obj);
+            }
+        }
 
-	/// <summary>
-	/// Creates the XML file (Encrypted)
-	/// </summary>
-	/// <param name="fileName">File name.</param>
-	/// <param name="obj">Object.</param>
-	protected static void CreateEXMLFile(string fileName, System.Object obj) {
-		XmlSerializer serializer = new XmlSerializer(obj.GetType());
-		StringWriter textWriter = new StringWriter();
+        /// <summary>
+        /// Creates the XML file (Encrypted)
+        /// </summary>
+        /// <param name="fileName">File name.</param>
+        /// <param name="obj">Object.</param>
+        protected static void CreateEXMLFile(string fileName, System.Object obj)
+        {
+            XmlSerializer serializer = new XmlSerializer(obj.GetType());
+            StringWriter textWriter = new StringWriter();
 
-		serializer.Serialize(textWriter, obj);
-		string encryptData = EncryptData(textWriter.ToString());
+            serializer.Serialize(textWriter, obj);
+            string encryptData = EncryptData(textWriter.ToString());
 
-		using(StreamWriter stream = new StreamWriter(XMLBaseFile._base_path + fileName + XMLBaseFile._extension, false)) {
-			stream.WriteLine(encryptData);
-		}
-	}
+            using (StreamWriter stream = new StreamWriter(XMLBaseFile._base_path + fileName + XMLBaseFile._extension, false))
+            {
+                stream.WriteLine(encryptData);
+            }
+        }
 
-	/// <summary>
-	/// Loads the XML file (NON-Encrypted)
-	/// </summary>
-	/// <returns>The XML file.</returns>
-	/// <param name="fileName">File name.</param>
-	/// <param name="type">Type.</param>
-	protected static System.Object LoadXMLFile(string fileName, System.Type type) {
-		XmlSerializer serializer = new XmlSerializer(type);
+        /// <summary>
+        /// Loads the XML file (NON-Encrypted)
+        /// </summary>
+        /// <returns>The XML file.</returns>
+        /// <param name="fileName">File name.</param>
+        /// <param name="type">Type.</param>
+        protected static System.Object LoadXMLFile(string fileName, System.Type type)
+        {
+            XmlSerializer serializer = new XmlSerializer(type);
 
-		using (FileStream stream = new FileStream(XMLBaseFile._base_path + fileName + XMLBaseFile._extension, FileMode.Open)) {
-			return serializer.Deserialize(stream) as System.Object;
-		}
-	}
-	
-	/// <summary>
-	/// Loads the XML file (Encrypted)
-	/// </summary>
-	/// <returns>The EXML file.</returns>
-	/// <param name="fileName">File name.</param>
-	/// <param name="type">Type.</param>
-	protected static System.Object LoadEXMLFile(string fileName, System.Type type) {
-		XmlSerializer serializer = new XmlSerializer(type);
+            using (FileStream stream = new FileStream(XMLBaseFile._base_path + fileName + XMLBaseFile._extension, FileMode.Open))
+            {
+                return serializer.Deserialize(stream) as System.Object;
+            }
+        }
 
-		using(StreamReader stream = new StreamReader(XMLBaseFile._base_path + fileName + XMLBaseFile._extension, false)) {
-			string decryptData = DecryptData(stream.ReadToEnd());
+        /// <summary>
+        /// Loads the XML file (Encrypted)
+        /// </summary>
+        /// <returns>The EXML file.</returns>
+        /// <param name="fileName">File name.</param>
+        /// <param name="type">Type.</param>
+        protected static System.Object LoadEXMLFile(string fileName, System.Type type)
+        {
+            XmlSerializer serializer = new XmlSerializer(type);
 
-			using (TextReader reader = new StringReader(decryptData)) {
-				return serializer.Deserialize(reader) as System.Object;
-			}
-		}
-	}
+            using (StreamReader stream = new StreamReader(XMLBaseFile._base_path + fileName + XMLBaseFile._extension, false))
+            {
+                string decryptData = DecryptData(stream.ReadToEnd());
 
-	/// <summary>
-	/// Encrypts the data.
-	/// </summary>
-	/// <returns>The data.</returns>
-	/// <param name="data">Data.</param>
-	private static string EncryptData(string data) {
-		byte[] key = UTF8Encoding.UTF8.GetBytes(XMLBaseFile._key);
+                using (TextReader reader = new StringReader(decryptData))
+                {
+                    return serializer.Deserialize(reader) as System.Object;
+                }
+            }
+        }
 
-		byte[] dataBytes = UTF8Encoding.UTF8.GetBytes(data);
+        /// <summary>
+        /// Encrypts the data.
+        /// </summary>
+        /// <returns>The data.</returns>
+        /// <param name="data">Data.</param>
+        private static string EncryptData(string data)
+        {
+            byte[] key = UTF8Encoding.UTF8.GetBytes(XMLBaseFile._key);
 
-		RijndaelManaged rDel = new RijndaelManaged();
-		rDel.Key = key;
-		rDel.Mode = CipherMode.ECB;
-		rDel.Padding = PaddingMode.PKCS7;
+            byte[] dataBytes = UTF8Encoding.UTF8.GetBytes(data);
 
-		ICryptoTransform iCTransform = rDel.CreateEncryptor();
+            RijndaelManaged rDel = new RijndaelManaged();
+            rDel.Key = key;
+            rDel.Mode = CipherMode.ECB;
+            rDel.Padding = PaddingMode.PKCS7;
 
-		byte[] encryptedBytes = iCTransform.TransformFinalBlock(dataBytes, 0, dataBytes.Length);
+            ICryptoTransform iCTransform = rDel.CreateEncryptor();
 
-		return Convert.ToBase64String(encryptedBytes, 0, encryptedBytes.Length);
-	}
+            byte[] encryptedBytes = iCTransform.TransformFinalBlock(dataBytes, 0, dataBytes.Length);
 
-	/// <summary>
-	/// Decrypts the data.
-	/// </summary>
-	/// <returns>The data.</returns>
-	/// <param name="data">Data.</param>
-	private static string DecryptData(string data) {
-		byte[] key = UTF8Encoding.UTF8.GetBytes(XMLBaseFile._key);
+            return Convert.ToBase64String(encryptedBytes, 0, encryptedBytes.Length);
+        }
 
-		byte[] dataBytes = Convert.FromBase64String(data);
+        /// <summary>
+        /// Decrypts the data.
+        /// </summary>
+        /// <returns>The data.</returns>
+        /// <param name="data">Data.</param>
+        private static string DecryptData(string data)
+        {
+            byte[] key = UTF8Encoding.UTF8.GetBytes(XMLBaseFile._key);
 
-		RijndaelManaged rDel = new RijndaelManaged();
-		rDel.Key = key;
-		rDel.Mode = CipherMode.ECB;
-		rDel.Padding = PaddingMode.PKCS7;
+            byte[] dataBytes = Convert.FromBase64String(data);
 
-		ICryptoTransform iCTransform = rDel.CreateDecryptor();
+            RijndaelManaged rDel = new RijndaelManaged();
+            rDel.Key = key;
+            rDel.Mode = CipherMode.ECB;
+            rDel.Padding = PaddingMode.PKCS7;
 
-		byte[] decryptedBytes = iCTransform.TransformFinalBlock(dataBytes, 0, dataBytes.Length);
+            ICryptoTransform iCTransform = rDel.CreateDecryptor();
 
-		return UTF8Encoding.UTF8.GetString(decryptedBytes, 0, decryptedBytes.Length);
-	}
+            byte[] decryptedBytes = iCTransform.TransformFinalBlock(dataBytes, 0, dataBytes.Length);
+
+            return UTF8Encoding.UTF8.GetString(decryptedBytes, 0, decryptedBytes.Length);
+        }
+    }
 }
