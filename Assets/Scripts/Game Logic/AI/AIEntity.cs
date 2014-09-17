0836214525bcc99442838caf6fa5a079846fa864
ablanchard17 <ablanchard1010@gmail.com>
2014-09-17 09:18:47 -0700

    Added:
    AITraits class
    AIEntity class

---
+++ Assets/Scripts/Game Logic/AI/AIEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts.Game_Logic.AI
+{
+    public class AIEntity : MonoBehaviour
+    {
+
+    }
+}
