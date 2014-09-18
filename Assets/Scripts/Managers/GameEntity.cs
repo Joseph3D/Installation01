bbc7049b8091b467fd938d1658f62c83d69fae5f
ablanchard17 <ablanchard1010@gmail.com>
2014-09-18 16:48:55 -0700

    implementation in progress.

--- Assets/Scripts/Managers/GameEntity.cs
+++ Assets/Scripts/Managers/GameEntity.cs
@@ -6,7 +6,26 @@ using UnityEngine;
 
 namespace Assets.Scripts.Managers
 {
+    /// <summary>
+    /// Represents an entity in the game
+    /// </summary>
     public class GameEntity : MonoBehaviour
     {
+        public string Name { get; protected set; }
+
+        public GameEntity(string EntityName)
+        {
+            Name = EntityName;
+        }
+
+        public void Start()
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
     }
 }
