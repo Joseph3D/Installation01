426497baaf9614393e4cea66d4def287aaf69ef1
ablanchard17 <ablanchard1010@gmail.com>
2014-09-24 08:55:37 -0700

    Added: CameraFreeFly.cs - MonoBehaviour component that gives the camera free look and flying controls Test Scene for AI Grid Normal map texture ( generated by unity )

--- Assets/Scripts/Game Logic/AI/AIEntity.cs
+++ Assets/Scripts/Game Logic/AI/AIEntity.cs
@@ -2,27 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using UnityEngine;
 
 namespace Assets.Scripts.Game_Logic.AI
 {
     public sealed class AIEntity : MonoBehaviour
     {
-        private CharacterController Controller;
-
-
-        public AIEntity()
-        {
-        }
+		private CharacterController Controller;
 
         public void Start()
         {
-
+			Controller = GetComponent<CharacterController>() as CharacterController;
         }
-
+		
         public void Update()
         {
+			Test_MoveForeward();
+		}
 
-        }
+		private void Test_MoveForeward()
+		{
+			Vector3 MoveVelocity = new Vector3(1,0,0);
+
+			Controller.SimpleMove(MoveVelocity);
+		}
     }
 }
--- Assets/Scripts/Game Logic/AI/AIManager.cs
+++ Assets/Scripts/Game Logic/AI/AIManager.cs
@@ -2,10 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
+using System.IO;
+using UnityEngine;
 
 namespace Assets.Scripts.Game_Logic.AI
 {
-    public sealed class AIManager
+	/// <summary>
+	/// This class will be attached to an empty GameObject
+	/// This class is responsible for managing a list of every single active AI entity in the world
+	/// As well as a central point for any other functions / utilities that AIEntities may need
+	/// </summary>
+	public sealed class AIManager :  MonoBehaviour
     {
+		private List<GameObject> AIEntityCollection;
+
+		
+		public void Start()
+		{
+			AIEntityCollection = new List<GameObject>();
+		}
+
+		public void Update()
+		{
+		}
+
+
+		private void UpdateAIEntityCollection()
+		{
+			throw new NotImplementedException();
+		}
     }
 }
