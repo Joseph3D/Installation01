4c062fdffaf0b01ddd747ada9b003238b7e0daf4
ablanchard17 <ablanchard1010@gmail.com>
2014-09-30 10:51:04 -0700

    Added:
    Stack based Ai state management
    
    better structure to UpdateAIState()
    
    AIState.SingleWaypoint is tested and working

--- Assets/Scripts/Game Logic/AI/AIEntity.cs
+++ Assets/Scripts/Game Logic/AI/AIEntity.cs
@@ -10,8 +10,11 @@ namespace Assets.Scripts.Game_Logic.AI
     public sealed class AIEntity : MonoBehaviour
     {
 		private CharacterController Controller;
+		
+		private Stack<AIState> StateStack;
 
 		private Stack<Vector3> Waypoints;
+		public int WaypointTolerance;
 
 		private AIManager    Manager;
 		private Ray          VisionRay;
@@ -21,21 +24,90 @@ namespace Assets.Scripts.Game_Logic.AI
 		public float Speed;
 		public float SprintSpeed;
 		private bool Sprinting;
-
-		public AIState State;
+	
 
         public void Start()
         {
 			InitializeInternals();
+			
+			WaypointTest();
+        }
+        
+        public void WaypointTest()
+        {
+        	Vector3 Position = transform.position;
+        	Position.x += 20;
+        	Position.z += 20;
+        	
+        	AddWaypoint(Position);
+        	PushState(AIState.SingleWaypoint);
         }
 		
         public void Update()
         {
-			MoveEntity(MovementDirection.Left);
+			UpdateAIState();
+			
+			DEBUG_Update();
+		}
+		
+		private void DEBUG_Update()
+		{
+			Debug.DrawRay(this.transform.position,transform.position + (Vector3.forward * 5),Color.green);
 		}
 
-		private void UpdateMovement() // currently unused
+
+		private void UpdateAIState()
 		{
+			switch(CurrentState())
+			{
+				case AIState.Idle:
+				{
+					break;
+				}
+				case AIState.SingleWaypoint:
+				{
+					Update_SingleWaypoint();
+					break;
+				}
+				case AIState.WaypointList:
+				{
+					Update_WaypointList();
+					break;
+				}
+			}
+		}
+		
+		private void Update_SingleWaypoint()
+		{
+			if(AtWaypoint(CurrentWaypoint(),WaypointTolerance))
+			{
+				RemoveCurrentState();
+			}
+			else
+			{
+				MoveEntityTowardsPoint(CurrentWaypoint());
+			}
+		}
+		
+		/// <summary>
+		/// Update behavior for WaypointList state
+		/// </summary>
+		private void Update_WaypointList()
+		{
+			if(AtWaypoint(CurrentWaypoint(),WaypointTolerance))
+			{
+				RemoveTopWaypoint();
+				return;
+			}
+			else
+			{
+				MoveEntityTowardsPoint(CurrentWaypoint());
+			}
+		}
+
+		private bool AtWaypoint(Vector3 Waypoint, int Tolerance)
+		{
+			return Physics.CheckSphere(Waypoint,Tolerance,1);
 		}
 
 		/// <summary>
@@ -116,7 +188,7 @@ namespace Assets.Scripts.Game_Logic.AI
 		/// Returns the next waypoint Stack.Peek()
 		/// </summary>
 		/// <returns>The waypoint.</returns>
-		private Vector3 NextWaypoint()
+		private Vector3 CurrentWaypoint()
 		{
 			return Waypoints.Peek();
 		}
@@ -130,6 +202,29 @@ namespace Assets.Scripts.Game_Logic.AI
 		{
 			return Waypoints.Count;
 		}
+		
+		/// <summary>
+		/// returns the current AIState via Stack.Peek
+		/// </summary>
+		/// <returns>The state.</returns>
+		public AIState CurrentState()
+		{
+			if(StateStack.Count > 0)
+			{
+				return StateStack.Peek();
+			}
+			return AIState.Idle;
+		}
+		
+		public void RemoveCurrentState()
+		{
+			StateStack.Pop();
+		}
+		
+		public void PushState(AIState State)
+		{
+			StateStack.Push(State);
+		}
 
 		/// <summary>
 		/// 1) Grabs CharacterController component for this entity
@@ -140,25 +235,12 @@ namespace Assets.Scripts.Game_Logic.AI
 		private void InitializeInternals()
 		{
 			Controller = GetComponent<CharacterController>() as CharacterController;
-
-			GameObject AIManagerGameObject = GameObject.FindGameObjectWithTag("AI Manager");
-
+			
 			Waypoints = new Stack<Vector3>();
-
-			if(!AIManagerGameObject)
-			{
-				throw new Exception("AI Manager GameObject not found in game world.");
-			}
-			else
-			{
-				Manager = AIManagerGameObject.GetComponent(typeof(AIManager)) as AIManager;
-				if(!Manager)
-				{
-					throw new Exception(@"AIManager component not found in 'AI Manager' GameObject");
-				}
-			}
-
-			Vector3 Position = transform.position;
+			
+			StateStack = new Stack<AIState>();
+			
+			StateStack.Push(AIState.Idle);
 		}	
     }
 }
