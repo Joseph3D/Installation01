54f2465f4fd55f8e62a23cdf2806a72f9232fb0e
ablanchard17 <ablanchard1010@gmail.com>
2014-10-04 17:07:21 -0700

    Added navmesh to test environment scene

---
+++ Assets/Scripts/Data/FileHotloader.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading;
+
+namespace Assets.Scripts.Data
+{
+    public sealed class FileHotloader
+    {
+    }
+}
