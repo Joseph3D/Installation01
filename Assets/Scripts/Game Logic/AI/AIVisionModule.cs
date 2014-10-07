c76033c723011d1c36b89d0dd0f899d859ddf0cb
ablanchard17 <ablanchard1010@gmail.com>
2014-10-07 09:32:55 -0700

    Added

---
+++ Assets/Scripts/Game Logic/AI/AIVisionModule.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts.Game_Logic.AI
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class AIVisionModule
+    {
+    }
+}
---
+++ Assets/Scripts/Helpers/EditorHelperObject.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts.Helpers
+{
+    public class EditorHelperObject : MonoBehaviour
+    {
+        private MeshRenderer Renderer;
+        private bool UpdateStarted;
+
+        public void Start()
+        {
+            UpdateStarted = false;
+
+            Renderer = GetComponent<MeshRenderer>() as MeshRenderer;
+        }
+
+        public void Update()
+        {
+            if (!UpdateStarted)
+            {
+                Renderer.enabled = false;
+                UpdateStarted = true;
+            }
+        }
+    }
+}
