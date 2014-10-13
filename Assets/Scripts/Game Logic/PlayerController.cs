0394fb66ac0422138ab5213e6949f64eaa97d3c4
ablanchard17 <ablanchard1010@gmail.com>
2014-10-13 11:41:21 -0700

    Added core player movement code

--- Assets/Scripts/Game Logic/PlayerController.cs
+++ Assets/Scripts/Game Logic/PlayerController.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using System.IO;
 using System.Threading;
 using System.Collections;
@@ -67,9 +68,107 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        // If the run button is set to toggle, then switch between walk/run speed. (We use Update for this...
+        // FixedUpdate is a poor place to use GetButtonDown, since it doesn't necessarily run every frame and can miss the event)
+        if (toggleRun && grounded && Input.GetButtonDown("Run"))
+            speed = (speed == walkSpeed ? runSpeed : walkSpeed);
+    }
 
+    void FixedUpdate()
+    {
+        UpdateMovement();
     }
     #endregion
+
+
+    private void UpdateMovement()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+        // If both horizontal and vertical are used simultaneously, limit speed (if allowed), so the total doesn't exceed normal move speed
+        float inputModifyFactor = (inputX != 0.0f && inputY != 0.0f && limitDiagonalSpeed) ? .7071f : 1.0f;
+
+        if (grounded)
+        {
+            bool sliding = false;
+            // See if surface immediately below should be slid down. We use this normally rather than a ControllerColliderHit point,
+            // because that interferes with step climbing amongst other annoyances
+            if (Physics.Raycast(myTransform.position, -Vector3.up, out hit, rayDistance))
+            {
+                if (Vector3.Angle(hit.normal, Vector3.up) > slideLimit)
+                    sliding = true;
+            }
+            // However, just raycasting straight down from the center can fail when on steep slopes
+            // So if the above raycast didn't catch anything, raycast down from the stored ControllerColliderHit point instead
+            else
+            {
+                Physics.Raycast(contactPoint + Vector3.up, -Vector3.up, out hit);
+                if (Vector3.Angle(hit.normal, Vector3.up) > slideLimit)
+                    sliding = true;
+            }
+
+            // If we were falling, and we fell a vertical distance greater than the threshold, run a falling damage routine
+            if (falling)
+            {
+                falling = false;
+                if (myTransform.position.y < fallStartLevel - fallingDamageThreshold)
+                    FallingDamageAlert(fallStartLevel - myTransform.position.y);
+            }
+
+            // If running isn't on a toggle, then use the appropriate speed depending on whether the run button is down
+            if (!toggleRun)
+                speed = Input.GetButton("Run") ? runSpeed : walkSpeed;
+
+            // If sliding (and it's allowed), or if we're on an object tagged "Slide", get a vector pointing down the slope we're on
+            if ((sliding && slideWhenOverSlopeLimit) || (slideOnTaggedObjects && hit.collider.tag == "Slide"))
+            {
+                Vector3 hitNormal = hit.normal;
+                moveDirection = new Vector3(hitNormal.x, -hitNormal.y, hitNormal.z);
+                Vector3.OrthoNormalize(ref hitNormal, ref moveDirection);
+                moveDirection *= slideSpeed;
+                playerControl = false;
+            }
+            // Otherwise recalculate moveDirection directly from axes, adding a bit of -y to avoid bumping down inclines
+            else
+            {
+                moveDirection = new Vector3(inputX * inputModifyFactor, -antiBumpFactor, inputY * inputModifyFactor);
+                moveDirection = myTransform.TransformDirection(moveDirection) * speed;
+                playerControl = true;
+            }
+
+            // Jump! But only if the jump button has been released and player has been grounded for a given number of frames
+            if (!Input.GetButton("Jump"))
+                jumpTimer++;
+            else if (jumpTimer >= antiBunnyHopFactor)
+            {
+                moveDirection.y = jumpSpeed;
+                jumpTimer = 0;
+            }
+        }
+        else
+        {
+            // If we stepped over a cliff or something, set the height at which we started falling
+            if (!falling)
+            {
+                falling = true;
+                fallStartLevel = myTransform.position.y;
+            }
+
+            // If air control is allowed, check movement but don't touch the y component
+            if (airControl && playerControl)
+            {
+                moveDirection.x = inputX * speed * inputModifyFactor;
+                moveDirection.z = inputY * speed * inputModifyFactor;
+                moveDirection = myTransform.TransformDirection(moveDirection);
+            }
+        }
+
+        // Apply gravity
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        // Move the controller, and set grounded true or false depending on whether we're standing on something
+        grounded = (controller.Move(moveDirection * Time.deltaTime) & CollisionFlags.Below) != 0;
+    }
 
 
     private void InitializeInternals()
@@ -86,6 +185,16 @@ public class PlayerController : MonoBehaviour
         rayDistance = controller.height * .5f + controller.radius;
         slideLimit = controller.slopeLimit - .1f;
         jumpTimer = antiBunnyHopFactor;
+    }
+
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        contactPoint = hit.point;
+    }
+
+    void FallingDamageAlert(float fallDistance)
+    {
+        throw new NotImplementedException();
     }
     #endregion
 }
