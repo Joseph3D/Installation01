e7dc5655d22f84e927b8b7f61fdb0eb5ad452e0c
ablanchard17 <ablanchard1010@gmail.com>
2014-10-13 15:50:53 -0700

    Added: projectile.cs

---
+++ Assets/Scripts/Game Logic/Projectile.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour
+{
+    #region Members
+    private Vector3 StartingPosition;
+    private float Velocity;
+    private float Weight;
+    private int Lifespan;
+    private Vector3 Direction;
+
+    private LineRenderer DebugPathRenderer;
+    #endregion
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
