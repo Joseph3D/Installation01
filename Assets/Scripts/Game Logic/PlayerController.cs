b0c0fb16e59e8b3e608e09a41e28e786c7c94f54
ablanchard17 <ablanchard1010@gmail.com>
2014-10-13 09:37:07 -0700

    Pre restructure commit:
    Added: player controller script

---
+++ Assets/Scripts/Game Logic/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Threading;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    #region Members
+    #endregion
+
+    #region Methods
+    #region MonoBehavior Methods
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+    #endregion
+    #endregion
+}
