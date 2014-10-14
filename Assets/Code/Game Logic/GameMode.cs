f1eb650a0724330230d95481ec6e26b1133a9ef9
ablanchard17 <ablanchard1010@gmail.com>
2014-10-14 09:31:12 -0700

    Added

---
+++ Assets/Code/Game Logic/GameMode.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameLogic
+{
+    public enum GameMode
+    {
+        DevelopmentTesting = 0,
+        Firefight = 1,
+    }
+}
---
+++ Assets/Code/Game Logic/ProjectileTraits.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+using Helpers;
+
+namespace GameLogic
+{
+    [XmlRoot]
+    public sealed class ProjectileTraits
+    {
+        [XmlElement]
+        public float Velocity { get; private set; }
+
+        [XmlElement]
+        public float Weight { get; private set; }
+
+        [XmlElement]
+        public float Lifespan { get; set; }
+
+        [XmlElement]
+        public float Damage { get; private set; }
+
+        [XmlElement]
+        public bool Homing { get; private set; }
+
+        [XmlElement]
+        public float MaxHomingTurn { get; private set; }
+
+        public static ProjectileTraits LoadFromFile(string TraitsFile)
+        {
+            if(File.Exists(TraitsFile))
+            {
+                ProjectileTraits LoadedTraits = XMLBaseFile.LoadXMLFile(TraitsFile, typeof(ProjectileTraits)) as ProjectileTraits;
+                return LoadedTraits;
+            }
+            return null;
+        }
+    }
+}
