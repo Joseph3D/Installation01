e4a50be65a05859bf1cac6ee5392bee3e9d28951
ablanchard17 <ablanchard1010@gmail.com>
2014-10-16 08:42:23 -0700

    tags added

--- Assets/Code/Game Logic/EntityTag.cs
+++ Assets/Code/Game Logic/EntityTag.cs
@@ -6,12 +6,14 @@ using System.Text;
 namespace GameLogic
 {
     [Flags]
-   public enum EntityTag
-   {
-       GameEntity = 0,
-       AIEntity = 1,
-       Player = 2,
-       Enemy = 3,
-       SpawnPoint = 4,
-   }
+    public enum EntityTag
+    {
+        GameEntity = 0,
+        AIEntity = 1,
+        Player = 2,
+        Enemy = 3,
+        SpawnPoint = 4,
+        Projectile = 5,
+        SceneryObject = 6,
+    }
 }
