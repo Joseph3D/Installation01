cb8d36a001f5aa834d9dcf0c7d312007d4022d16
ablanchard17 <ablanchard1010@gmail.com>
2014-10-15 17:43:34 -0700

    added

---
+++ Assets/Code/Game Logic/EntityTag.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameLogic
+{
+   public enum EntityTags
+   {
+       GameEntity = 0,
+       AIEntity = 1,
+       Player = 2,
+       Enemy = 3,
+       SpawnPoint = 4,
+
+   }
+}
