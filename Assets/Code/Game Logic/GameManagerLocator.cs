7113dbb511ea129438b60e0032d21aa5e3fae392
ablanchard17 <ablanchard1010@gmail.com>
2014-10-22 14:18:37 -0700

    Implemented GameManagerLocator

---
+++ Assets/Code/Game Logic/GameManagerLocator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using System.IO;
+using System.Threading;
+using System.Collections;
+using System.Collections.Generic;
+using GameLogic;
+using Helpers;
+
+namespace GameLogic
+{
+    public static class GameManagerLocator
+    {
+        private static GameManager _Manager;
+        private static bool ManagerFound = false;
+
+        public static GameManager Manager
+        {
+            get
+            {
+                return _Manager;
+            }
+        }
+
+        static GameManagerLocator()
+        {
+            if(!ManagerFound)
+            {
+                _Manager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+                ManagerFound = true;
+            }
+        }
+    }
+}
