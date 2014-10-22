2d7662c9e92d0de87ada231c769f2f40cd990b4b
ablanchard17 <ablanchard1010@gmail.com>
2014-10-21 21:48:59 -0700

    Added

---
+++ Assets/Code/Helpers/Cache.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Helpers
+{
+    public class Cache<T>
+    {
+        private List<T> CacheList;
+
+        public Cache()
+        {
+            CacheList = new List<T>();
+        }
+    }
+}
