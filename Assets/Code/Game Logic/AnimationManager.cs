d4d74c7b6154da6d7871d859e31796edafb6fbbb
ablanchard17 <ablanchard1010@gmail.com>
2014-10-28 09:47:40 -0700

    implementation in progress

--- Assets/Code/Game Logic/AnimationManager.cs
+++ Assets/Code/Game Logic/AnimationManager.cs
@@ -9,16 +9,20 @@ namespace GameLogic
 {
     public class AnimationManager : MonoBehaviour
     {
+        public string[] AnimationNames;
+
         private Animator Animator;
+
+        private Dictionary<string,int> AnimationHashes;
 
         public void Awake()
         {
-
+            InitializeInternals();
         }
 
         public void Start()
         {
-
+            
         }
 
         public void Update()
@@ -29,6 +33,31 @@ namespace GameLogic
         private void InitializeInternals()
         {
             Animator = GetComponent<Animator>();
+
+            AnimationHashes = new Dictionary<string, int>();
+
+            CacheAnimationHashes();
+        }
+
+        public void PlayAnimation(string Name)
+        {
+        
+        }
+
+        public void PlayAnimation(int AnimationHash)
+        {
+
+        }
+
+        private void CacheAnimationHashes()
+        {
+            if(AnimationNames.Length > 0)
+            {
+                for(int i = 0; i < AnimationNames.Length; ++i)
+                {
+                    AnimationHashes.Add(AnimationNames[i], Animator.StringToHash(AnimationNames[i]));
+                }
+            }
         }
     }
 }
