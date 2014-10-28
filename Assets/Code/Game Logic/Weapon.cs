919d4513da6c5962d04b1c42bb138362816be3b9
ablanchard17 <ablanchard1010@gmail.com>
2014-10-28 14:54:13 -0700

    Added:
    Weapon
    WeaponTraits

---
+++ Assets/Code/Game Logic/Weapon.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Threading;
+using System.Collections;
+using System.Collections.Generic;
+using GameLogic;
+using Helpers;
+
+using UnityDebug = UnityEngine.Debug;
+using UnityRandom = UnityEngine.Random;
+
+namespace GameLogic
+{
+    public sealed class Weapon : MonoBehaviour
+    {
+        private GameManager Manager;
+
+        public void Awake()
+        {
+            InitializeInternals();
+        }
+        public void Start()
+        {
+
+        }
+        public void Update()
+        {
+
+        }
+        public void FixedUpdate()
+        {
+
+        }
+
+        private void InitializeInternals()
+        {
+            Manager = GameManagerLocator.Manager;
+        }
+    }
+}
---
+++ Assets/Code/Game Logic/WeaponTraits.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Threading;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Serialization;
+using GameLogic;
+using Helpers;
+
+using UnityDebug  = UnityEngine.Debug;
+using UnityRandom = UnityEngine.Random;
+
+namespace GameLogic
+{
+    [Serial]
+    public sealed class WeaponTraits
+    {
+    }
+}
