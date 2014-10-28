601b6b3e5a51a24804e7e96c255aa0bc1539be4c
ablanchard17 <ablanchard1010@gmail.com>
2014-10-28 09:34:22 -0700

    Added

---
+++ Assets/Code/Game Logic/AnimationManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Threading;
+using System.Collections;
+using System.Collections.Generic;
+using GameLogic;
+using Helpers;
+namespace GameLogic
+{
+    public class AnimationManager : MonoBehaviour
+    {
+        private Animator Animator;
+
+        public void Awake()
+        {
+
+        }
+
+        public void Start()
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
+
+        private void InitializeInternals()
+        {
+            Animator = GetComponent<Animator>();
+        }
+    }
+}
