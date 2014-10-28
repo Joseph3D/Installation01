8d0403add7ff3b24db9a6b8f5d26d1d3a84d9cb7
ablanchard17 <ablanchard1010@gmail.com>
2014-10-28 12:14:49 -0700

    added

---
+++ Assets/Code/Game Logic/AudioEmitter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using System.IO;
+using System.Threading;
+using System.Collections;
+using System.Collections.Generic;
+using GameLogic;
+using Helpers;
+
+namespace GameLogic
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class AudioEmitter : MonoBehaviour
+    {
+        private GameManager Manager;
+
+        public void Awake()
+        {
+
+        }
+
+        public void Start()
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
+
+
+        private void InitializeInternals()
+        {
+            Manager = GameManagerLocator.Manager;
+        }
+    }
+}
