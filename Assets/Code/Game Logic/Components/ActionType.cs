a861a762c61e0fc6da0bcc1820ccfecfcad63d74
ablanchard17 <ablanchard1010@gmail.com>
2014-10-31 19:45:16 -0700

    Added Action and ActionType

---
+++ Assets/Code/Game Logic/Components/ActionType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Code.Game_Logic.Components
+{
+    /// <summary>
+    /// Represents the type of behavior that this action will trigge
+    /// </summary>
+    [Flags]
+    public enum ActionType
+    {
+        PlaySoundEffect = 0,
+        SpawnEntity = 1 << 0,
+        DespawnEntity = 1 << 1,
+        DespawnAllWithTag = 1 << 2,
+    }
+}
